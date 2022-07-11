cb3e379202a7f08c3b215b35e7ae78c93e444c8a
Doddy MATABARO <matabarododdy@gmail.com>
2022-07-11 10:27:52 +0200

    first contribution

---
+++ people/doddymatabaro.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Doddy Matabaro',
+    github: 'https://github.com/DoddyMatabaro',
+    email: 'matabarododdy@gmail.com',
+    twitter: '@https://twitter.com/matabaro_doddy',
+    facebook: 'https://www.facebook.com/ahashabyoshi',
+    linkedin: 'https://www.linkedin.com/in/doddy-matabaro-0a9666211'
+  };
